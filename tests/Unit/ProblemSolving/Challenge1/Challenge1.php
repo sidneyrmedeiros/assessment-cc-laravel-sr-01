bad3ddd1826678dabc4588416beb89f0cc747d30
Thiago Galvani <tgalvani@fullstacklabs.co>
2022-12-12 10:37:14 -0300

    feat: adding the problem solving 1

---
+++ tests/Unit/ProblemSolving/Challenge1/Challenge1.php
@@ -0,0 +1,11 @@
+<?php
+namespace Tests\Unit\ProblemSolving\Challenge1;
+
+class Challenge1
+{
+    public static function numberFractionsCalculator($numbers) {
+        // @TODO implement challenge
+    }
+}
+
+?>
