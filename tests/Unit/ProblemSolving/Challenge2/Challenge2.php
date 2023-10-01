bea76e51d91a92a67478b8a35c950d41e18c65d9
Sidney Ricardo Medeiros <sidneyrmedeiros@gmail.com>
2023-10-01 15:11:06 -0300

    Chanllenge 2

--- tests/Unit/ProblemSolving/Challenge2/Challenge2.php
+++ tests/Unit/ProblemSolving/Challenge2/Challenge2.php
@@ -1,11 +1,26 @@
 <?php
+
 namespace Tests\Unit\ProblemSolving\Challenge2;
+
+use Exception;
 
 class Challenge2
 {
-    public static function diceFacesCalculator($dice1, $dice2, $dice3) {
-        // @TODO implement challenge
+    public static function diceFacesCalculator($dice1, $dice2, $dice3)
+    {
+        $dices = [$dice1, $dice2, $dice3];
+        $countValues = array_count_values($dices);
+        foreach ($countValues as $key => $value) {
+
+            if ($key > 6 || $key < 1) {
+                throw new Exception('Dice out of number range');
+
+            }
+
+            if ($value > 1) {
+                return $key * $value;
+            }
+        }
+        return max($dices);
     }
 }
-
-?>
