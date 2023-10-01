ee5855277dfb2d0f5967e2355a013f1e73911a8e
Sidney Ricardo Medeiros <sidneyrmedeiros@gmail.com>
2023-10-01 14:56:21 -0300

    Add Battles features

--- app/Http/Controllers/BattleController.php
+++ app/Http/Controllers/BattleController.php
@@ -5,30 +5,26 @@ namespace App\Http\Controllers;
 use App\Services\BattleService;
 use App\Services\MonsterService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Response;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class BattleController extends Controller
 {
-
     /**
-     *
-     * @var $battleService
+     * @var
      */
     protected $battleService;
 
     /**
-     *
-     * @var $monsterService
+     * @var
      */
     protected $monsterService;
 
     /**
      * BattleService constructor.
      *
-     * @param BattleService $battleService
-     * @param MonsterService $monsterService
-     *
+     * @param  BattleService  $battleService
+     * @param  MonsterService  $monsterService
      */
     public function __construct(BattleService $battleService, MonsterService $monsterService)
     {
@@ -40,16 +36,51 @@ class BattleController extends Controller
      * Get all battles.
      *
      * @return JsonResponse
-     *
      */
     public function index(): JsonResponse
     {
         return response()->json(
             [
-                'data' => $this->battleService->getAll()
+                'data' => $this->battleService->getAll(),
             ],
             Response::HTTP_OK
         );
     }
 
+    /**
+     * Create new battle.
+     *
+     * @param  Request  $request
+     * @return JsonResponse
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $newBattle = $request->only([
+            'monsterA',
+            'monsterB',
+        ]);
+
+        return response()->json(
+            [
+                'data' => $this->battleService->createBattle($newBattle),
+            ],
+            Response::HTTP_CREATED
+        );
+    }
+
+    /**
+     * Remove a batte.
+     *
+     * @param  Request  $request
+     * @return JsonResponse
+     */
+    public function remove(Request $request): JsonResponse
+    {
+        $battleId = $request->route('id');
+        $result = $this->battleService->getBattleById($battleId);
+
+        $this->battleService->removeBattle($battleId);
+
+        return response()->json('', Response::HTTP_NO_CONTENT);
+    }
 }
--- app/Interfaces/BattleRepositoryInterface.php
+++ app/Interfaces/BattleRepositoryInterface.php
@@ -2,7 +2,6 @@
 
 namespace App\Interfaces;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 
 interface BattleRepositoryInterface
